f437a841d99aa6c31bdc284d9c510538bf6191c1
mogy-h <rapter.mg@gmail.com>
2021-08-30 23:41:58 +0900

    いくつかチャートを作成

---
+++ src/features/covid/Cards/Cards.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "./Cards.module.css";
+import CountUp from "react-countup";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+
+import { GiHastyGrave } from "react-icons/gi";
+import { MdLocalHospital } from "react-icons/md";
+import { AiFillLike } from "react-icons/ai";
+
+import { useAppSelector } from "../../../app/hooks";
+import { selectData } from "../covidSlice";
+
+const Cards: React.FC = () => {
+  const data = useAppSelector(selectData);
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={1} justifyContent="center">
+        <Grid item xs={12} md={3} component={Card} className={styles.infected}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              <MdLocalHospital />
+              感染者数
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={data.confirmed.value}
+                duration={1.5}
+                separator=","
+              />
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item xs={12} md={3} component={Card} className={styles.recovered}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              <AiFillLike /> 治療済の人数
+            </Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={data.recovered.value} duration={1.5} separator="," />
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item xs={12} md={3} component={Card} className={styles.deaths}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              <GiHastyGrave />
+              死亡者数
+            </Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={data.deaths.value} duration={1.5} separator="," />
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Cards;
